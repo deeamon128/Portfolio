e8d3d1c4efcdfa42f6628ed54768fb17e9e34831
deeamon128 <andreeambobariu@gmail.com>
2021-12-12 20:26:52 +0000

    Banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
+import ContactBanner from '../components/ContactBanner';
 import HeroSection from '../components/HeroSection';
 import ProjectSection from '../components/ProjectSection';
 import ServiceSection from '../components/ServiceSection';
@@ -10,6 +11,7 @@ const Home = () => (
     <AboutSection />
     <ServiceSection />
     <ProjectSection />
+    <ContactBanner />
   </div>
 );
 
